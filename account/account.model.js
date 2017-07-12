69dfa5acfad3297a55a339ff04037158f3e925f0
jmatesanz <jonathan.matesanz@trivago.com>
2017-07-12 10:24:04 +0200

    Revert ":)"
    This reverts commit 44100929a6a0a53c3f1685a138ef38a3ac342cb1.

--- account/account.model.js
+++ account/account.model.js
@@ -1,10 +1,7 @@
 import bookshelf from '../db';
 
-const AccountModel = bookshelf.Model.extend({
+const accountModel = bookshelf.Model.extend({
   tableName: 'account',
 });
 
-
-const accountModel = bookshelf.model('AccountModel', AccountModel); // Register model to bookshelf registry plugin to avoid circular dependency
-
-export { accountModel }; // Export registered model
+module.exports = bookshelf.model('accountModel', accountModel);
